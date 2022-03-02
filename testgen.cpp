5d29df271c8ab06e386e38bca6e18be4149223db
Abhinandan <64205626+abhinandanmishra1@users.noreply.github.com>
2022-03-02 22:27:14 +0530

    Update testgen.cpp

--- testgen.cpp
+++ testgen.cpp
@@ -1,8 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int size[]={10,20,30,40,50,60,70,100,200,300,400,500};
+int sizer[]={100,400,500,20000,300000,400000,500000,600000,700000,800000,900000,1000000};
 int kkk=0;
+string generateString(int size){
+    string res(size,'a');
+    int i=0;
+    while(i<size){
+        int ind=rand()%26;
+        res[i++]=char('a'+ind);
+    }
+    return res;
+}
 void generateArray(int m){
     for(int i=0;i<m;i++){
         int n=rand()%100005+1;
@@ -13,21 +22,16 @@ void generateArray(int m){
 
 void generateTests(){
     // now call that functions which you want 
-    int n=size[kkk++];
-    int m=rand()%n+10;
-    cout<<n<<" "<<m<<endl;
-    for(int i=0;i<n;i++){
-        generateArray(m);
-    }
+
+    
 }
 
 int main(){
-	const char* inp[]={"input1.txt","input2.txt","input3.txt","input5.txt","input6.txt","input7.txt","input8.txt","input9.txt","input10.txt","input11.txt","input12.txt"};
-	const char* outp[]={"output1.txt","output2.txt","output3.txt","output5.txt","output6.txt","output7.txt","output8.txt","output9.txt","output10.txt","output11.txt","output12.txt"};
+	const char* inp[]={"input1.txt","input2.txt","input3.txt","input4.txt","input5.txt","input6.txt","input7.txt","input8.txt","input9.txt","input10.txt","input11.txt","input12.txt"};
+	const char* outp[]={"output1.txt","output2.txt","output3.txt","output4.txt","output5.txt","output6.txt","output7.txt","output8.txt","output9.txt","output10.txt","output11.txt","output12.txt"};
     for(int i=0;i<12;i++){
         freopen(inp[i],"w",stdout);
         generateTests();
     }
 	return 0;
 }
-
